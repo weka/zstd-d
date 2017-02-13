2e45c1a5326fc607776d07917d5b6cb0ac14246c
Jonathan Shamir <jonathan@weka.io>
2017-02-13 14:18:57 +0200

    Rewrite examples/streaming to use the new classes

--- examples/streaming.d
+++ examples/streaming.d
@@ -2,33 +2,43 @@ import zstd;
 static import std.file;
 static import std.stdio;
 
-void main(string[] args)
-{
-    if (args.length <= 1)
+void main(string[] args) {
+    import std.functional : partial;
+    import std.algorithm : max;
+
+    if (args.length <= 1) {
         throw new Exception("Must specify filename");
+    }
+    immutable bufSize = max(StreamCompressor.recommendedOutSize, StreamDecompressor.recommendedOutSize);
+    void[] buffer = new ubyte[bufSize];
+    const(void)[] original = std.file.read(args[1]);
 
-    ubyte[] original = cast(ubyte[])std.file.read(args[1]);
-    ubyte[][] contents1;
-    contents1 ~= original[0..$ / 2];
-    contents1 ~= original[$ / 2..$];
+    // compress
+    std.stdio.writef("Start compressing... ");
+    auto compressor = new StreamCompressor(6);
+    void[] compressed;
+    compressed.reserve(original.length);
+    foreach(buf; compressor.applyCompress(original, buffer)) {
+        compressed ~= buf;
+    }
+    foreach(buf; compressor.applyFinish(buffer)) {
+        compressed ~= buf;
+    }
+    std.stdio.writefln("Done.");
 
-    ubyte[] compressed;
-    auto compressor = new Compressor();
-    foreach (src; contents1)
-        compressed ~= compressor.compress(src);
-    compressed ~= compressor.flush();
+    // decompress
+    std.stdio.writef("Start decompressing... ");
+    auto decompressor = new StreamDecompressor();
+    void[] decompressed;
+    decompressed.reserve(original.length);
+    foreach(buf; decompressor.applyDecompress(compressed, buffer)) {
+        decompressed ~= buf;
+    }
+    foreach(buf; decompressor.applyFinish(buffer)) {
+        //assert(false);
+        decompressed ~= buf;
+    }
+    std.stdio.writefln("Done.");
 
-    std.stdio.writefln("original size:   %d", original.length);
-    std.stdio.writefln("compressed size: %d", compressed.length);
-
-    ubyte[][] contents2;
-    contents2 ~= compressed[0..$ / 2];
-    contents2 ~= compressed[$ / 2..$];
-
-    ubyte[] decompressed;
-    auto uncompressor = new Decompressor();
-    foreach (src; contents2)
-        decompressed ~= uncompressor.decompress(src);
-
-    std.stdio.writeln(original == decompressed);
+    assert(original == decompressed);
 }
